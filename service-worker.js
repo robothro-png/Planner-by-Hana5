1a01734ff8eb3334ff020fb293f91ab34bc1f846
robothro-png <robothro@gmail.com>
2026-04-15 09:42:19 +0700

    Add service worker for caching files

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "planner-cache-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
